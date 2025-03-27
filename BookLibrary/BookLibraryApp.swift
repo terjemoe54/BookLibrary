218b5c967896071b590cf3f23802110184d49ca2
Terje Moe <terjemoe54@hotmail.com>
2025-03-27 06:59:54 +0100

    Initial Commit

---
+++ BookLibrary/BookLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookLibraryApp.swift
+//  BookLibrary
+//
+//  Created by Terje Moe on 27/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct BookLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
