a221d2979f68c2918862510eb9bc2c4ab6282b6d
Terje Moe <terjemoe54@hotmail.com>
2025-03-28 09:22:17 +0100

    Finish Chapter 60

--- BookLibrary/ContentView.swift
+++ BookLibrary/ContentView.swift
@@ -24,7 +24,7 @@ struct ContentView: View {
   
     @State private var selectedAuthor: Author?
     @State private var isEditingAuthor = false
-    @State private var isSheetPresented = false
+    @State private var isSheetingPresented = false
         
     enum SortOption {
         case forward, reverse
@@ -33,14 +33,85 @@ struct ContentView: View {
     var body: some View {
         NavigationStack {
             VStack {
-                if sortedAuthors.isEmpty {
+                if !sortedAuthors.isEmpty {
                     EmptyAuthorsView()
                 } else {
+                    Picker(
+                        "Sort by",
+                        selection: $sortOption) {
+                            Text("Forward").tag(SortOption.forward)
+                            Text("Reverse").tag(SortOption.reverse)
+                    }
+                        .pickerStyle(SegmentedPickerStyle())
+                        .padding()
                     
+                    
+                    List {
+                        ForEach(sortedAuthors) { author in
+                            DisclosureGroup("\(author.name) - ^[\(author.books.count) books](inflect: true)") {
+                                ForEach(author.books) { book in
+                                    HStack {
+                                        Text(book.title)
+                                        Spacer()
+                                        Button("📝") {
+                                            simpleUpdateBook(book)
+                                        }
+                                    }
+                                }
+                            }
+                            .swipeActions {
+                                Button(role: .destructive) {
+                                    // delete author
+                                    deleteAuthor(author)
+                                } label:  {
+                                    Label("Delete", systemImage: "trash")
+                                }
+                                
+                                Button {
+                                    // edit
+                                } label:  {
+                                    Label("Edit", systemImage: "pencil")
+                                }
+                                .tint(.blue)
+                            }
+                        }
+                    }
                 }
             }
             .navigationTitle("Authors & Books")
+            .toolbar {
+                Button("➕ Add") {
+    //                selectedAuthor = Author(name: "")
+                    selectedAuthor = Author(name: "TEST NAME")
+                    isSheetingPresented = true
+                }
+            }
+            .sheet(isPresented: $isSheetingPresented) {
+                if let author = selectedAuthor {
+                    EditAuthorView(author: author)
+                }
+            }
         }
+    }
+    
+    func simpleUpdateBook(_ book: Book) {
+        book.title += ["😎", "😤", "🥱"].randomElement() ?? "😎"
+        try? modelContext.save()
+    }
+    
+    func deleteAuthor(_ author: Author) {
+        modelContext.delete(author)
+        try? modelContext.save()
+    }
+    
+}
+
+
+struct EditAuthorView: View {
+    @Bindable var author: Author
+    
+    var body: some View {
+        Text("EditAuthorView")
     }
 }
 
@@ -49,3 +120,4 @@ struct ContentView: View {
     ContentView()
         .modelContainer(for: [Author.self, Book.self])
 }
+
